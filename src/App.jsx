39d68119d4b5d257fa5e1d63137023b4d1f7d5e0
Dawood Trumboo <107537057+dawoodtrumboo@users.noreply.github.com>
2023-10-01 18:56:49 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useState, useEffect } from "react";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { TabBtn} from "./Components";
+import { TabBtn } from "./Components";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import data from './constants/data.json';
-import { Mumbai, NewYork, Paris, London } from "./Pages";
-import { faHourglass } from '@fortawesome/free-regular-svg-icons'
+import { Mumbai, NewYork, Paris, London, PropertyPage } from "./Pages";
 
 function App() {
+  // State for selected city and city data
   const [selectCity, setSelectCity] = useState("New York");
   const [cityData, setCityData] = useState(data[selectCity]);
 
+  // Load selected city from local storage on initial render
   useEffect(() => {
     const storedCity = localStorage.getItem("selectedCity");
     if (storedCity) {
@@ -18,11 +19,13 @@ function App() {
     }
   }, []);
 
+  // Save selected city to local storage and update city data when it changes
   useEffect(() => {
     localStorage.setItem("selectedCity", selectCity);
     setCityData(data[selectCity]);
   }, [selectCity]);
 
+  // Function to handle city selection
   const handleSelectCity = (city) => {
     setSelectCity(city);
   }
@@ -30,6 +33,7 @@ function App() {
   return (
     <Router>
       <div className="App bg-[#F4F5FD] h-screen flex flex-col items-center px-[120px] !overflow-x-hidden ">
+        {/* Header */}
         <div className="w-[340px] flex flex-col items-center text-gray-700 ">
           <h1 className="text-[28px] font-bold">Featured Listed Property</h1>
           <p className="text-[10px] text-center font-semibold">
@@ -38,6 +42,7 @@ function App() {
           </p>
         </div>
 
+        {/* City Tabs */}
         <div className="w-full mt-10 flex justify-between ">
           <div className="space-x-5">
             <Link to='/'>
@@ -69,6 +74,7 @@ function App() {
               />
             </Link>
           </div>
+          {/* View All Button */}
           <button className="bg-[#ECECFB] rounded-full text-xs font-bold 
             px-4 py-2 text-[#3639E4] border-[#3639E4] border-[1px] space-x-1">
             <span>View All</span>
@@ -76,21 +82,17 @@ function App() {
           </button>
         </div>
 
+        {/* City Content */}
         <div className='w-full flex flex-col items-center gap-5 my-5'>
           <Routes>
             <Route path="/" element={<NewYork cityData={cityData} />}/>
             <Route path="/mumbai" element={<Mumbai cityData={cityData} />}/>
             <Route path="/paris" element={<Paris cityData={cityData} />}/>
             <Route path="/london" element={<London cityData={cityData} />}/>
+            <Route path="/property/:id" element={<PropertyPage cityData={cityData} />} />
           </Routes>
         </div>
-        <div className='text-center my-5'>
-          <button className="bg-[#3639E4] rounded-full
-          px-4 py-2 text-white font-bold text-[14px] space-x-1">
-              <FontAwesomeIcon icon={faHourglass} size='lg' color='#fff'/>
-              <span>Show More</span>
-          </button>
-          </div>
+        
       </div>
     </Router>
   );
